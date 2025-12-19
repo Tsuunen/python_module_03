9bacbf1bfccba47af02b65a0708bd6c3fe75ed8a
Remi Laforgue <relaforg@z3r6p2.42lyon.fr>
2025-12-19 14:37:54 +0100

    ex1

---
+++ ex1/ft_score_analytics.py
@@ -0,0 +1,25 @@
+import sys
+
+if (__name__ == "__main__"):
+    scores = []
+    print("=== Player Score Analytics ===")
+    if (len(sys.argv) <= 1):
+        print(
+            "No scores provided. Usage: python3 ft_score_analytics.py \
+<score1> <score2> ...")
+    else:
+        try:
+            i: int = 0
+            for arg in sys.argv:
+                if (i > 0):
+                    scores.append(int(arg))
+                i += 1
+            print("Scores processed:", scores)
+            print("Total players:", len(scores))
+            print("Total score:", sum(scores))
+            print("Average score:", sum(scores) / len(scores))
+            print("High score:", max(scores))
+            print("Low score:", min(scores))
+            print("Score range:", max(scores) - min(scores))
+        except ValueError as e:
+            print("Error:", e)
